aa2f2f77f3405f3d3889990ebe70caf81304d5bb
Groot2theMoon <dongdong0615@postech.ac.kr>
2022-12-27 18:03:19 +0900

    20221227_1

---
+++ conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from djangogrom.users.models import User
+from djangogrom.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
