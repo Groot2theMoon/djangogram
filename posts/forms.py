4162e2e220c70698130bd5e6d44b35509d5a8ac2
Groot2theMoon <dongdong0615@postech.ac.kr>
2022-12-29 01:13:21 +0900

    20221229 7-4 comment_gen

--- posts/forms.py
+++ posts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 class CreatePostForm(forms.ModelForm):
     class Meta:
@@ -8,5 +8,12 @@ class CreatePostForm(forms.ModelForm):
 
         labels = {
             "caption": "내용",
-            "image": "사진진"
+            "image": "사진"
         }
+
+class CommentForm(forms.ModelForm):
+    contents = forms.CharField(widget=forms.Textarea, label="")
+
+    class Meta:
+        model = Comment
+        fields = ["contents"]
