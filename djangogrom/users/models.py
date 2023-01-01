03ee77902fec7d867bf6815b72e8c80b3e0be532
Groot2theMoon <dongdong0615@postech.ac.kr>
2023-01-01 18:06:52 +0900

    20230101 8-4

--- djangogrom/users/models.py
+++ djangogrom/users/models.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
@@ -18,23 +17,16 @@ class User(AbstractUser):
     """
 
     #: First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
-    first_name = None  # type: ignore
-    last_name = None  # type: ignore
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
     user_name = models.CharField(blank=True, max_length=255)
     profile_photo = models.ImageField(blank=True)
-    bio = models.URLField(blank=True)
-    email = models.TextField(blank=True)
+    website = models.URLField(blank=True)
+    bio = models.TextField(blank=True)
+    email = models.CharField(blank=True, max_length=255)
     phone_number = models.CharField(blank=True, max_length=255)
     gender = models.CharField(blank=True, choices=GENDER_CHOICES, max_length=255)
     followers = models.ManyToManyField("self")
     following = models.ManyToManyField("self")
 
     def get_absolute_url(self):
-        """Get url for user's detail view.
-
-        Returns:
-            str: URL for user detail.
-
-        """
         return reverse("users:detail", kwargs={"username": self.username})
